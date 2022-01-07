43bec192b08206019855d701bf4ce6b9742013c2
kplbl <51096422+kplbl@users.noreply.github.com>
2022-01-07 09:07:54 +0100

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
 import './index.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './layout/Navbar';
+import PostList from './layout/PostList';
+import Post from './components/Post';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App container mx-auto">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<PostList type={'topstories'} />} />
+          <Route path="/new" element={<PostList type={'newstories'} />} />
+          <Route path="/post/:postId" element={<Post />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
