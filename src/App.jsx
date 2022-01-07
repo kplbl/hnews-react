45dd86a2a4e8edf5be970f6e4cd33e412bf51168
kplbl <51096422+kplbl@users.noreply.github.com>
2022-01-07 18:22:24 +0100

    new routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './layout/Navbar';
 import PostList from './layout/PostList';
 import Post from './components/Post';
+import Wellno from './components/Wellno';
 
 function App() {
   return (
@@ -13,7 +14,14 @@ function App() {
           <Routes>
             <Route path="/" element={<PostList type={'topstories'} />} />
             <Route path="/new" element={<PostList type={'newstories'} />} />
+            <Route path="/best" element={<PostList type={'beststories'} />} />
+            <Route path="/top" element={<PostList type={'topstories'} />} />
+            <Route path="/ask" element={<PostList type={'askstories'} />} />
+            <Route path="/show" element={<PostList type={'showstories'} />} />
+            <Route path="/jobs" element={<PostList type={'jobstories'} />} />
             <Route path="/post/:postId" element={<Post />} />
+            <Route path="/submit" element={<Wellno />} />
+            <Route path="/login" element={<Wellno />} />
           </Routes>
         </Router>
       </div>
