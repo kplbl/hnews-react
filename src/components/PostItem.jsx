127a993b287cea48e84f60e65caeff2384c5cc2f
kplbl <51096422+kplbl@users.noreply.github.com>
2022-01-07 09:09:46 +0100

    styled, router navigation, timeago component

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,15 +1,34 @@
-import { Link } from 'react-router-dom';
-function PostItem({ post }) {
-  const { title, by, id, score, url } = post;
+import Timeago from 'react-timeago';
+import { useNavigate } from 'react-router-dom';
 
-  console.log(post);
+function PostItem({ post }) {
+  const { title, by, id, score, url, time, descendants } = post;
+
+  const navigate = useNavigate();
+
+  let domain = null;
+  try {
+    let { hostname } = new URL(url);
+    domain = hostname;
+  } catch (error) {
+    domain = null;
+  }
+
   return (
-    <div className="block">
+    <div className="block mb-4 bg-slate-100" onClick={() => navigate(`/post/${id}`)}>
       <div>
-        <span>{title}</span>
-        <span>{post.url}</span>
+        <span className="font-semibold mr-5">{title}</span>
+        {domain && (
+          <span className="text-sm">
+            <a href={url}>({domain})</a>
+          </span>
+        )}
       </div>
-      <div>{post.score}</div>
+      <div className="text-sm">
+        {score} points by {by} <Timeago date={time * 1000} />
+        {' | '}
+        {descendants && <span>{descendants} comments</span>}
+      </div>
     </div>
   );
 }
