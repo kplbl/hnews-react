c615cc50cd9221354d8ec1bd46a86d0cd90f44f3
kplbl <51096422+kplbl@users.noreply.github.com>
2022-01-07 16:47:07 +0100

    styling

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,35 +1,35 @@
 import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
-    <nav className="flex justify-between">
-      <div className="flex gap-2 py-5">
-        <Link to="/" className="text-lg mr-5">
-          Hacker News
+    <nav className="flex justify-between border-b-black border-b mb-2 mt-4">
+      <div className="flex gap-2 py-0">
+        <Link to="/" className="text-lg font-bold mr-5 py-2 bg-orange-500 px-2">
+          Hacker News <span className="font-light italic text-gray-700">lurker</span>
         </Link>
 
-        <Link to="/new" className="navbar-link">
+        <Link to="/new" className="navbar-link py-2">
           new
         </Link>
-        <Link to="/past" className="navbar-link">
+        <Link to="/past" className="navbar-link py-2">
           past
         </Link>
-        <Link to="/newcomments" className="navbar-link">
+        <Link to="/newcomments" className="navbar-link py-2">
           comments
         </Link>
-        <Link to="/ask" className="navbar-link">
+        <Link to="/ask" className="navbar-link py-2">
           ask
         </Link>
-        <Link to="/show" className="navbar-link">
+        <Link to="/show" className="navbar-link py-2">
           show
         </Link>
-        <Link to="/jobs" className="navbar-link">
+        <Link to="/jobs" className="navbar-link py-2">
           jobs
         </Link>
-        <Link to="/submit" className="navbar-link">
+        <Link to="/submit" className="navbar-link py-2">
           submit
         </Link>
       </div>
-      <Link to="/login" className="py-5 navbar-link">
+      <Link to="/login" className="py-2 navbar-link">
         Login
       </Link>
     </nav>
