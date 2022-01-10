62d9acfedff0ad32be31881bf6ae4205ceb33fe4
kplbl <51096422+kplbl@users.noreply.github.com>
2022-01-10 19:15:57 +0100

    page, setpage state, set as props to postlist and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './index.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 import Navbar from './layout/Navbar';
 import PostList from './layout/PostList';
 import Post from './components/Post';
@@ -7,19 +8,41 @@ import Wellno from './components/Wellno';
 import User from './components/User';
 
 function App() {
+  const [page, setPage] = useState(0);
   return (
     <div className="App h-full w-full">
-      <div className="container mx-auto">
+      <div className="md:container mx-auto">
         <Router>
-          <Navbar />
+          <Navbar page={page} setPage={setPage} />
           <Routes>
-            <Route path="/" element={<PostList type={'topstories'} />} />
-            <Route path="/new" element={<PostList type={'newstories'} />} />
-            <Route path="/best" element={<PostList type={'beststories'} />} />
-            <Route path="/top" element={<PostList type={'topstories'} />} />
-            <Route path="/ask" element={<PostList type={'askstories'} />} />
-            <Route path="/show" element={<PostList type={'showstories'} />} />
-            <Route path="/jobs" element={<PostList type={'jobstories'} />} />
+            <Route
+              path="/"
+              element={<PostList type={'topstories'} page={page} setPage={setPage} />}
+            />
+            <Route
+              path="/new"
+              element={<PostList type={'newstories'} page={page} setPage={setPage} />}
+            />
+            <Route
+              path="/best"
+              element={<PostList type={'beststories'} page={page} setPage={setPage} />}
+            />
+            <Route
+              path="/top"
+              element={<PostList type={'topstories'} page={page} setPage={setPage} />}
+            />
+            <Route
+              path="/ask"
+              element={<PostList type={'askstories'} page={page} setPage={setPage} />}
+            />
+            <Route
+              path="/show"
+              element={<PostList type={'showstories'} page={page} setPage={setPage} />}
+            />
+            <Route
+              path="/jobs"
+              element={<PostList type={'jobstories'} page={page} setPage={setPage} />}
+            />
             <Route path="/post/:postId" element={<Post />} />
             <Route path="/user/:userId" element={<User />} />
 
