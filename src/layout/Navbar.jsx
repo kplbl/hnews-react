9b53756c777eba1cb09e22e7b9c02c273c6899cf
kplbl <51096422+kplbl@users.noreply.github.com>
2022-01-10 10:45:46 +0100

    changed title styling

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
     <nav className="flex justify-between border-b-black border-b mb-2 mt-4">
       <div className="flex gap-2 py-0">
         <Link to="/" className="text-lg font-bold mr-5 py-2 bg-orange-500 px-2">
-          Hacker News <span className="font-light italic text-gray-700">lurker</span>
+          Hacker News <span className="italic text-orange-200">lurker</span>
         </Link>
 
         <Link to="/new" className="navbar-link py-2">
