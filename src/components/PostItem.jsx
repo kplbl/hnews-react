a60842ac78eee33098599cf58b0040919ba60b50
kplbl <51096422+kplbl@users.noreply.github.com>
2022-01-22 12:20:46 +0100

    styling, new link

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -15,19 +15,27 @@ function PostItem({ post }) {
   return (
     <div className="block mb-2 text-gray-700 p-1">
       <div>
-        <span className="font-semibold mr-5">
+        <span className="font-semibold mr-5 hover:underline">
           <Link to={`/post/${id}`}>{title}</Link>{' '}
         </span>
         {domain && (
-          <span className="text-sm">
+          <span className="text-sm hover:underline">
             <a href={url}>({domain})</a>
           </span>
         )}
       </div>
       <div className="text-sm">
-        {score} points by <Link to={`/user/${by}`}>{by}</Link> <Timeago date={time * 1000} />
+        {score} points by{' '}
+        <Link className="hover:underline" to={`/user/${by}`}>
+          {by}
+        </Link>{' '}
+        <Timeago date={time * 1000} />
         {' | '}
-        {descendants && <span>{descendants} comments</span>}
+        {descendants && (
+          <Link className="hover:underline" to={`/post/${id}`}>
+            {descendants} comments
+          </Link>
+        )}
       </div>
     </div>
   );
